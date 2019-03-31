3a3f5df47af41032a3dabcf6d0290c26fc0bcc04
mattia <mattiaiamundo@gmail.com>
2019-03-31 14:12:26 +0200

    completed Class skeleton for Model

--- src/main/java/it/polimi/sw2019/model/weapon_power/Shadowstep.java
+++ src/main/java/it/polimi/sw2019/model/weapon_power/Shadowstep.java
@@ -1,7 +1,10 @@
 package it.polimi.sw2019.model.weapon_power;
 
+import it.polimi.sw2019.model.Space;
+
 public class Shadowstep implements Power{
     //[Optional for Cyberblade] move yourself of 1 square
+    private Space moveto;
 
     @Override
     public void usePower(){
